7d91c7e5e85155c1b553bc4c99e121ba72f61652
John Doe <gafne321@mail.ru>
2022-10-20 12:02:29 +0300

    Лаба

---
+++ lb5shilC/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите сумму вклада ");
+double a = Convert.ToDouble(Console.ReadLine());
+if( a < 100)
+{
+    a *= 1.05;
+    Console.WriteLine("Вы получите {0} ",a);
+}
+if (a > 100 && a < 200)
+{
+    a *= 1.07;
+    Console.WriteLine("Вы получите {0} ", a);
+}
+if( a > 200 )
+{
+    a *= 1.1;
+    Console.WriteLine("Вы получите {0} ", a);
+}
